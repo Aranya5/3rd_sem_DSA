3fd8e30992b5f7e1cbe3951d29b38ca50dcf420c
Aranya5 <aranyabasu5@gmail.com>
2025-04-11 21:47:21 +0530

    Binary Search Tree

--- bst.c
+++ bst.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 };
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
         printf("Memory allocation failed\n");
         exit(1);
     }
@@ -17,54 +20,64 @@ struct Node* createNode(int data) {
     newNode->right = NULL;
     return newNode;
 }
-struct Node* insert(struct Node* root, int data) {
-    if (root == NULL) {
+struct Node *insert(struct Node *root, int data)
+{
+    if (root == NULL)
+    {
         return createNode(data);
     }
-    
-    if (data < root->data) {
+
+    if (data < root->data)
+    {
         root->left = insert(root->left, data);
-    } else {
+    }
+    else
+    {
         root->right = insert(root->right, data);
     }
     return root;
 }
-void inorderTraversal(struct Node* root) {
-    if (root == NULL) return;
+void inorderTraversal(struct Node *root)
+{
+    if (root == NULL)
+        return;
     inorderTraversal(root->left);
     printf("%d ", root->data);
     inorderTraversal(root->right);
 }
 
-void freeTree(struct Node* root) {
-    if (root == NULL) return;
+void freeTree(struct Node *root)
+{
+    if (root == NULL)
+        return;
     freeTree(root->left);
     freeTree(root->right);
     free(root);
 }
 
-int main() {
+int main()
+{
     int val;
-    struct Node* root = NULL;
-    while(1)
-    {  int ch;
+    struct Node *root = NULL;
+    while (1)
+    {
+        int ch;
         printf("Enter 1 to Enter\n");
-        scanf("%d",&ch);
-        switch(ch)
+        scanf("%d", &ch);
+        switch (ch)
         {
-            case 1:
-              {
-                printf("Enter value to insert in the tree :");
-                scanf("%d",&val);
-                root = insert(root, val);
-                inorderTraversal(root);
-                printf("\n");
-              }
-                 break;
+        case 1:
+        {
+            printf("Enter value to insert in the tree: ");
+            scanf("%d", &val);
+            root = insert(root, val);
+            inorderTraversal(root);
+            printf("\n");
+        }
+        break;
 
-            default:
+        default:
             exit(0);
-          
         }
     }
 
