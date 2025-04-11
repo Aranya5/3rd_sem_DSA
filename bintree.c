cfbb0a101b35a68f8c5096507fdaf9bbe4b6f280
Aranya5 <aranyabasu5@gmail.com>
2025-04-11 21:27:42 +0530

    Binary Tree

--- bintree.c
+++ bintree.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct node
+{
     int data;
     struct node *left;
     struct node *right;
-}bnode;
+} bnode;
 
-bnode *createNode(int val){
-    bnode *new = (bnode*)malloc(sizeof(int));
-    if(new != NULL){
-    new->data = val;
-    new->right = NULL;
-    new->left = NULL;
+bnode *createNode(int val)
+{
+    bnode *new = (bnode *)malloc(sizeof(int));
+    if (new != NULL)
+    {
+        new->data = val;
+        new->right = NULL;
+        new->left = NULL;
     }
     return new;
 }
 
-int main(){
+int main()
+{
     bnode *root = NULL;
-    int x,ch;
+    int x, ch;
     printf("Enter value of ROOT node");
-    scanf("%d",&x);
+    scanf("%d", &x);
     root = createNode(x);
-    if(root->data != NULL){
-        while(1){
-        printf("\n Enter 1 to insert the RIGHT node \n Enter 2 to insert the LEFT node \n Enter 3 to go back to node \n Enter 4 to EXIT");
-        scanf("%d",&ch);
-        switch(ch){
+    if (root->data != NULL)
+    {
+        while (1)
+        {
+            printf("\n Enter 1 to insert the RIGHT node \n Enter 2 to insert the LEFT node \n Enter 3 to go back to node \n Enter 4 to EXIT");
+            scanf("%d", &ch);
+            switch (ch)
+            {
             case 1:
-            
+            }
         }
     }
-
+    else
+    {
+        printf("Memory not allocated");
+        return 0;
     }
-    
-
-
 }
-
