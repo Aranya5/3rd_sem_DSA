663c36b60855986141a2144837ba25658aeb8647
Aranya5 <aranyabasu5@gmail.com>
2025-04-16 23:06:16 +0530

    Bubble Insersion and Heapsort

--- sorting.c
+++ sorting.c
@@ -1,83 +1,82 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void improved_buble_sort(int arr[],int size)
+void improved_buble_sort(int arr[], int size)
 {
-    int bound = size,t,i,temp,count=0;
+    int bound = size, t, i, temp, count = 0;
     do
     {
-        t=0;
-        for(i=0; i<bound-1; i++)
+        t = 0;
+        for (i = 0; i < bound - 1; i++)
         {
             count++;
-            if(arr[i] > arr[i+1])
+            if (arr[i] > arr[i + 1])
             {
                 temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-                t=i;
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                t = i;
             }
         }
-        bound = t+1;
-    } while (t>0);
-     printf("Number of comparisions %d\n", count);   
+        bound = t + 1;
+    } while (t > 0);
+    printf("Number of comparisions %d\n", count);
 }
 
-void insertion_sort(int arr[],int size)
+void insertion_sort(int arr[], int size)
 {
-    int i,j,key,count=0;
-    for(i=1; i<size; i++)
+    int i, j, key, count = 0;
+    for (i = 1; i < size; i++)
     {
         key = arr[i];
-        j=i-1;
-        while(j>=0 && arr[j]>key)
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
         {
-             count++;
-            arr[j+1]=arr[j];
+            count++;
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1]=key; 
-       
+        arr[j + 1] = key;
     }
-     printf("Number of comparisions %d\n", count);
+    printf("Number of comparisions %d\n", count);
 }
 
 void heapify(int arr[], int size, int i)
 {
     int largest = i;
-    int left = 2*i+1;
-    int right = 2*i+2;
-    if(left<size && arr[left]>arr[largest])
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < size && arr[left] > arr[largest])
     {
         largest = left;
     }
-    if(right<size && arr[right]>arr[largest])
+    if (right < size && arr[right] > arr[largest])
     {
         largest = right;
     }
-    if(largest != i)
+    if (largest != i)
     {
         int temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
-        heapify(arr,size,largest);
+        heapify(arr, size, largest);
     }
 }
 
 void heap_sort(int arr[], int size)
 {
     int i;
-    int count =0;
-    for(i=(size/2)-1; i>=0; i-- )
+    int count = 0;
+    for (i = (size / 2) - 1; i >= 0; i--)
     {
-        heapify(arr,size,i);
+        heapify(arr, size, i);
     }
-    for(i=size-1;i>0;i--)
+    for (i = size - 1; i > 0; i--)
     {
         int temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
-        heapify(arr,i,0);
+        heapify(arr, i, 0);
         count++;
     }
     printf("Number of comparisions %d\n", count);
@@ -85,25 +84,25 @@ void heap_sort(int arr[], int size)
 void display(int arr[], int size)
 {
     int i;
-    for(i=0;i<size;i++)
+    for (i = 0; i < size; i++)
     {
-        printf("%d, ",arr[i]);
+        printf("%d, ", arr[i]);
     }
     printf("\n");
 }
 
 int main()
 {
-    int A[]={10,34,5,96,67};
-    int B[]={10,20,30,40,50};
-    int C[]={50,40,30,20,10};
-    display(A,5);
+    int A[] = {10, 34, 5, 96, 67};
+    int B[] = {10, 20, 30, 40, 50};
+    int C[] = {50, 40, 30, 20, 10};
+    display(A, 5);
 
-    improved_buble_sort(A,5);
-    display(A,5);
-    insertion_sort(A,5);
-    display(A,5);
-    heap_sort(A,5);
-    display(A,5);
+    improved_buble_sort(A, 5);
+    display(A, 5);
+    insertion_sort(A, 5);
+    display(A, 5);
+    heap_sort(A, 5);
+    display(A, 5);
     return 0;
 }
